b2b3968329fe67d2f7622e0498595ceef44ac8a5
Your Name <you@example.com>
2021-10-29 02:13:51 +0200

    blingdogz mint logic

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
         if (ethereum) {
             web3 = new Web3(ethereum);
             account = (await ethereum.request({ method: 'eth_requestAccounts' }))[0];
-            contract = new web3.eth.Contract(JSON.parse(FROGS_ABI), "0xa3b7CEe4e082183E69a03Fc03476f28b12c545A7");
+            contract = new web3.eth.Contract(JSON.parse(FROGS_ABI), "0xB7089c672d6831bC6Bfb2A458e83296a99175308");
             const res = await contract.methods.totalSupply().call();
             $("#minted").text(res);
             $("#connect").hide();
@@ -17,12 +17,13 @@ $(document).ready(() => {
     $("#mint").on("click touchstart", async () => {
         if (web3) {
             const quantity = web3.utils.toBN($("#quantity").val());
-            const price = web3.utils.toBN("45000000000000000");
             const [presale, paused] = await Promise.all([contract.methods.presaleOngoing().call(), contract.methods.paused().call()]);
             if (presale && paused) {
+                const price = web3.utils.toBN("38800000000000000");
                 contract.methods.mintPresale(quantity).send({ value: price.mul(quantity), from: account });
             }
             else {
+                const price = web3.utils.toBN("44000000000000000");
                 contract.methods.mint(quantity).send({ value: price.mul(quantity), from: account });
             }
         }
